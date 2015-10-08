e3a2f30f090671a19a3ba3991a9c62336c6becd2
Stefan K. Berg <stefan.k.berg@ericsson.com>
2015-10-08 18:06:00 +0200

    Updated logic for selecting Ubuntu repo for mirroring
    A slight improvement to Michal Skalski's original concept just
    to filter out repos where updates are in progress (which would
    leave us hanging).
    
    Change-Id: Id641b3aa82b991c23b5742d1f64ff79cfbbd708c
    Signed-off-by: Stefan K. Berg <stefan.k.berg@ericsson.com>

---
+++ fuel/build/f_isoroot/f_repobuild/select_ubuntu_repo.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+RSYNC="rsync -4 --contimeout 5 --no-motd --list-only"
+
+# try to choose close ubuntu mirror which support rsync protocol
+# https://bugs.launchpad.net/fuel/+bug/1459252
+
+# A mindor modificiation of Michal Skalski's original Makefile version
+# to remove repos very update is in progress - which is also why the
+# script retries for an hour should no successful candidate have been
+# found.
+
+cnt=0
+while [ $cnt -lt 60 ]
+do
+    for url in $(curl -s http://mirrors.ubuntu.com/mirrors.txt)
+    do
+        host=$(echo $url | cut -d'/' -f3)
+        if $RSYNC "${host}::ubuntu/." &> /dev/null
+        then
+            if ! $RSYNC "${host}::ubuntu/Archive-Update-in-Progress*" &> /dev/null
+            then
+                echo "$host"
+                exit 0
+            fi
+        fi
+    done
+    cnt=$[cnt + 1]
+    sleep 60
+done
+exit 1
