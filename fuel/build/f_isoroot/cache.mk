f50dae48fff98bd828a2b87af61eed7c78b8bdae
Stefan K. Berg <stefan.k.berg@ericsson.com>
2015-10-15 12:51:26 +0200

    Improved caching functionality and tracability
    In order to enable the caching of more build artifacts than the Fuel
    iso during CI builds, the caching functionality and the CI build
    frontend "build.sh" has been rewritten.
    
    The build.sh script will now rely on the "make cache" functionality of
    the top Makefile to make sure that the build is using cache handling.
    
    The underlying cache logic is implemented in "cache.mk" for the top
    makefile and those recursive levels that do not produce cachable
    artifacts themselves in that they are only calling the SUBDIRS of
    their lower level (like f_isoroot).
    
    All "leaf" Makefiles will however need to implement three cache
    targets in their top Makefile (for visibility):
    
    clean-cache:
            Clean all files relating to the handling of caches.
    get-cache:
            Attempt to fetch a cached artifact using a SHA1 key.
    put-cache:
            Store a built artifact into the cache.
    
    They can just implement a simple "no-op" functionality for thes
    targets if they do not have any use of the caching functionality.
    
    If they are to use the caching functionality, they must make sure to
    implement a logic that produces a SHA1 hash based on the source of the
    data they are to cache - for upstream repos one suggestion is to use
    the commit ID of the used upstream HEAD. For examples, see the top
    Makefile that implements this logic for the Fuel ISO build, taking
    into consideration the commit IDs of all the upstream repositories
    used.
    
    To improve tracability, the root directory of the ISO will contain the
    file gitinfo.txt, which is meant to detail the upstream repo and
    commit ID used for all upstream dependencies of a build. If you are
    adding additional upstream dependencies, make sure to use the
    repo_info.sh tool to add this data.
    
    The cache tool cache.sh has no notion of the cache data it is storing
    - from the tools perspective, cache data is just a binary blob piped
    in or out of the tool.
    
    This blob is stored by the cache tool at the cache location as
    <SHA1>.blob, together with an associated meta file <SHA1>.meta.
    
    The cache meta file currently holds just one line:
    
        Expires: <epoch time>
    
    This file is expected to be used to iterate through the cache objects
    and retire those who's expiry date has passed. Currently objects will
    always have an expiry date of two weeks into the future, but down the
    road the "cache put" functionality may be amended with an optional
    "age" argument that can set a different expiration time.
    
    New tools in this commit:
    
      cache.sh - the cache logic
      repo_info.sh - the (optionally recursive) repo information logger
    
    Change-Id: I8a40546c21febeecc9de6d82c0ceb6bc60b04205
    Signed-off-by: Stefan K. Berg <stefan.k.berg@ericsson.com>

--- fuel/build/f_isoroot/cache.mk
+++ fuel/build/f_isoroot/cache.mk
@@ -1,5 +1,4 @@
 ##############################################################################
-# Copyright (c) 2015 Ericsson AB and others.
 # stefan.k.berg@ericsson.com
 # jonas.bjurel@ericsson.com
 # All rights reserved. This program and the accompanying materials
@@ -11,13 +10,9 @@
 #############################################################################
 # Cache operations - only used when building through ci/build.sh
 #
-# This is the global cache implementation, providing the main target "cache"
-# which is called from ci/build.sh, and recursively calling the cache
-# operations clean-cache, get-cache and put-cache on all $(SUBDIRS).
+# This file is only meant for a top Makefile which is only calling its
+# own SUBDIRS, without building any cachable artifact by itself.
 #############################################################################
-
-
-export CACHETOOL := $(BUILD_BASE)/cache.sh
 
 # Call sub caches
 SUBGETCACHE = $(addsuffix .getcache,$(SUBDIRS))
@@ -32,24 +27,11 @@ SUBCLEANCACHE = $(addsuffix .cleancache,$(SUBDIRS))
 $(SUBCLEANCACHE): %.cleancache:
 	$(MAKE) -C $* -f Makefile clean-cache
 
-# Overlay implementation:
-#   - clean
-#   - clean cache identities
-#   - get caches
-#   - build iso
-#   - store caches
-.PHONY: cached-all
-cached-all: clean clean-cache $(SUBCLEANCACHE) get-cache $(SUBGETCACHE) iso put-cache $(SUBPUTCACHE)
-	@echo "Cached build is complete"
+.PHONY: get-cache
+get-cache: $(SUBGETCACHE)
 
+.PHONY: put-cache
+put-cache: $(SUBPUTCACHE)
 
-# cache: The target for ci/build.sh
-.PHONY: cache
-cache:
-	@if [ -z "${CACHEBASE}" ]; then \
-		echo "CACHEBASE not set, are you really building through build.sh?"; \
-		exit 1; \
-	fi
-	@docker version >/dev/null 2>&1 || (echo 'No Docker installation available'; exit 1)
-	@make -C docker
-	docker/runcontext $(DOCKERIMG) $(MAKE) $(MAKEFLAGS) cached-all
+.PHONY: clean-cache
+clean-cache: $(SUBCLEANCACHE)
