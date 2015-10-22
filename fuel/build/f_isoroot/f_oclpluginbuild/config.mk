a7e1e9e416f08a1f897dc66daa0be8185d2b3643
Jonas Bjurel <jonas.bjurel@ericsson.com>
2015-10-22 21:54:26 +0200

    Integration of OpenContrail with Fuel 6.1 DESCRIPTION: - Builds OpenContrail fuel-plugin - Builds OpenContrail debian packages - Try out the build!
    TODO:
    - Deployment needs to be verified
    - OpenContrail repo versions in manifest.xml needs to be nailed down, currently
      master - NOT FIXED, version 2.2 currently doesnt build!
    - ODL has temporarilly been disabled due to build problems - needs to go back
      before merge.
    - Add CentOS support
    - Support for building with different target system kernel, compared to the
      build kernel.
    - Suport DPDK vrouter?
    
    CREDITS:
    All credits for this patch should go to Michal Skalski
    
    OTHERS:
    
    JIRA: FUEL-81
    DO NOT MERGE
    NOT VERIFIED
    
    Change-Id: I45578707d88cccc6a4dbda578047ff8068bdc209
    Signed-off-by: Jonas Bjurel <jonas.bjurel@ericsson.com>

---
+++ fuel/build/f_isoroot/f_oclpluginbuild/config.mk
@@ -0,0 +1,24 @@
+##############################################################################
+# Copyright (c) 2015 Ericsson AB and others.
+# jonas.bjurel@ericsson.com
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the Apache License, Version 2.0
+# which accompanies this distribution, and is available at
+# http://www.apache.org/licenses/LICENSE-2.0
+##############################################################################
+
+# Fuel Plugin repo/branch
+#
+export OCL_BRANCH="stable/2.1"
+export OCL_REPO="https://github.com/openstack/fuel-plugin-contrail"
+
+# Build options
+#
+# Uncomment The line below to in order to build a DPDK accellerated vRouter -
+# Not yet supported
+# CONTRAIL_DPDK=1
+
+# OpenContrail repos
+#
+# NOTE THE OPEN CONTRAIL REPO SPECIFICATION IS FOUND AT:
+# ===== ./manifest.xml =====
