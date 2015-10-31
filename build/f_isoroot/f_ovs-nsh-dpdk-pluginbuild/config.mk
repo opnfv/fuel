d71f58e51755e99084107c1c8ada3321e2600002
Jonas Bjurel <jonas.bjurel@ericsson.com>
2015-10-31 12:36:09 +0100

    Plugin builder for OVS-NSH-DPDK
    This plugin builder builds the OVS-NSH-DPDK plugin such
    that it can be automatically deployed off-line (part of the .iso)
    
    READY TO MERGE
    
    Change-Id: Ied8e0a8d01baea932acc5eb3398cec48e44d5c04
    Signed-off-by: Jonas Bjurel <jonas.bjurel@ericsson.com>

---
+++ build/f_isoroot/f_ovs-nsh-dpdk-pluginbuild/config.mk
@@ -0,0 +1,12 @@
+##############################################################################
+# Copyright (c) 2015 Ericsson AB and others.
+# jonas.bjurel@eicsson.com
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the Apache License, Version 2.0
+# which accompanies this distribution, and is available at
+# http://www.apache.org/licenses/LICENSE-2.0
+##############################################################################
+
+OVS_NSH_DPDK_BRANCH=373740ac0ad9de0c948edf7b4e35b86a8e6b2745
+OVS_NSH_DPDK_REPO=https://review.openstack.org/openstack/fuel-plugin-ovs
+OVS_NSH_DPDK_CHANGE=
