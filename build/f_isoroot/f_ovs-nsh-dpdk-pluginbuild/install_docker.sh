87d7954aa9a99bffbe35516c26e269fd3385f9d4
Ruijing Guo <ruijing.guo@intel.com>
2015-12-03 01:47:50 +0000

    Docker based build for ovs and rebase to Fuel 7.0
    Change-Id: I05ccabdb329dff31a52b3cd6c1b60148bee8efe9
    Signed-off-by: Ling Yu <ling.y.yu@intel.com>
    Signed-off-by: Ruijing Guo <ruijing.guo@intel.com>

---
+++ build/f_isoroot/f_ovs-nsh-dpdk-pluginbuild/install_docker.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+# From prepare-build-env.sh of Fuel
+# Check if docker is installed
+if hash docker 2>/dev/null; then
+  echo "Docker binary found, checking if service is running..."
+  ps cax | grep docker > /dev/null
+  if [ $? -eq 0 ]; then
+    echo "Docker is running."
+  else
+    echo "Process is not running, starting it..."
+    sudo service docker start
+  fi
+else
+  # Install docker repository
+  # Check that HTTPS transport is available to APT
+  if [ ! -e /usr/lib/apt/methods/https ]; then
+    sudo apt-get update
+    sudo apt-get -y install -y apt-transport-https
+  fi
+  # Add the repository to APT sources
+  echo deb http://mirror.yandex.ru/mirrors/docker/ docker main | sudo tee /etc/apt/sources.list.d/docker.list
+  # Import the repository key
+  sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 36A1D7869245C8950F966E92D8576A8BA88D21E9
+  # Install docker
+  sudo apt-get update
+  sudo apt-get -y install lxc-docker-1.5.0
+fi
