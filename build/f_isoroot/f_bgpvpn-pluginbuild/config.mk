1502860d5fb60bb6f29b580410b7cad0d2d8c54f
Jonas Bjurel <jonas.bjurel@ericsson.com>
2015-12-10 20:59:52 +0100

    Integration of ODL BGP-VPN plugin
    READY TO MERGE
    JIRA: FUEL-48
    
    Change-Id: Idc44966f32caf8728580bac8bcc89981f7c987ba
    Signed-off-by: Jonas Bjurel <jonas.bjurel@ericsson.com>

---
+++ build/f_isoroot/f_bgpvpn-pluginbuild/config.mk
@@ -0,0 +1,12 @@
+##############################################################################
+# Copyright (c) 2015 Ericsson AB and others.
+# jonas.bjurel@eicsson.com
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the Apache License, Version 2.0
+# which accompanies this distribution, and is available at
+# http://www.apache.org/licenses/LICENSE-2.0
+##############################################################################
+
+BGPVPN_BRANCH=32e4b36fa2d37dabb53cc3ee71960e2162f64a43
+BGPVPN_REPO="https://github.com/openstack/fuel-plugin-bgpvpn.git"
+OVS_NSH_DPDK_CHANGE=
