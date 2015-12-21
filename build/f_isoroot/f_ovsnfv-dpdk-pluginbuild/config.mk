35221244a9aa556135bcc89414d153edce5b348e
Jonas Bjurel <jonas.bjurel@ericsson.com>
2015-12-21 21:36:30 +0100

    Added Fuel plug-in builder for OVSNFV
    JIRA FUEL-36
    
    Change-Id: I354919bbed08dc2a449fa49334ca26d05e057676
    Signed-off-by: Jonas Bjurel <jonas.bjurel@ericsson.com>

---
+++ build/f_isoroot/f_ovsnfv-dpdk-pluginbuild/config.mk
@@ -0,0 +1,12 @@
+##############################################################################
+# Copyright (c) 2015 Ericsson AB and others.
+# jonas.bjurel@eicsson.com
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the Apache License, Version 2.0
+# which accompanies this distribution, and is available at
+# http://www.apache.org/licenses/LICENSE-2.0
+##############################################################################
+
+OVSNFV_DPDK_BRANCH=7cbf4b0e881c5c2ac1aa689b3212f54105a3ba4a
+OVSNFV_DPDK_REPO=https://github.com/mrmarkgray/fuel-plugin-ovsnfv
+OVSNFV_DPDK_CHANGE=
