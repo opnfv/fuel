29d1b12952cc43d8270fae6374eeeef911fdf10b
Jonas Bjurel <jonas.bjurel@ericsson.com>
2016-01-05 14:29:16 +0100

    Integration of vsperf plugin
    NOT VERIFIED
    DO NOT MERGE
    
    Change-Id: Ia3b01c0de1081ce24e4db361ba6e77475e8ea291
    Signed-off-by: Jonas Bjurel <jonas.bjurel@ericsson.com>

---
+++ build/f_isoroot/f_vsperfpluginbuild/config.mk
@@ -0,0 +1,12 @@
+##############################################################################
+# Copyright (c) 2015 Ericsson AB and others.
+# jonas.bjurel@eicsson.com
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the Apache License, Version 2.0
+# which accompanies this distribution, and is available at
+# http://www.apache.org/licenses/LICENSE-2.0
+##############################################################################
+
+VSPERF_BRANCH=4256eef4e237ce9409d8cbffe9237f37a315d513
+VSPERF_REPO=https://github.com/billyom/fuel-plugin-vsperf.git
+VSPERF_CHANGE=
