68e3d4b00350e8a27029905ce9279351e68ada9f
Mark D. Gray <mark.d.gray@intel.com>
2016-01-07 15:23:03 +0000

    f_ovsnfv-dpdk-pluginbuild: Update repo location
    The code for fuel-plugin-ovsnfv has been moved to
    the ovsnfv OPNFV project repository. This patch
    updates the OPNFV fuel plugin builder to use this
    location.
    
    Change-Id: I61299fab2ae93fa90a7b5fdb3cdb07b64cfde4c1
    Signed-off-by: Mark D. Gray <mark.d.gray@intel.com>

--- build/f_isoroot/f_ovsnfv-dpdk-pluginbuild/config.mk
+++ build/f_isoroot/f_ovsnfv-dpdk-pluginbuild/config.mk
@@ -7,6 +7,7 @@
 # http://www.apache.org/licenses/LICENSE-2.0
 ##############################################################################
 
-OVSNFV_DPDK_BRANCH=d13417a93c4df1d4db8f86f23ae4d42a056b5de8
-OVSNFV_DPDK_REPO=https://github.com/mrmarkgray/fuel-plugin-ovsnfv
+OVSNFV_DPDK_BRANCH=stable/brahmaputra
+OVSNFV_DPDK_REPO=https://gerrit.opnfv.org/gerrit/p/ovsnfv.git
+OVSNFV_DPDK_DIR=fuel-plugin-ovsnfv
 OVSNFV_DPDK_CHANGE=
