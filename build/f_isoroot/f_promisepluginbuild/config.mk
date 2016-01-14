cc6e053b4cdcafc0ad7129da20b3119058f99365
Jonas Bjurel <jonas.bjurel@ericsson.com>
2016-01-14 17:15:40 +0100

    Plugin-builder for Promise
    DO NOT MERGE
    NOT VERIFIED
    
    JIRA: FUEL-52
    
    Change-Id: I7313e1c4e76501975455128509055bb5f3c2b178
    Signed-off-by: Jonas Bjurel <jonas.bjurel@ericsson.com>

---
+++ build/f_isoroot/f_promisepluginbuild/config.mk
@@ -0,0 +1,12 @@
+##############################################################################
+# Copyright (c) 2015 Ericsson AB and others.
+# jonas.bjurel@eicsson.com
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the Apache License, Version 2.0
+# which accompanies this distribution, and is available at
+# http://www.apache.org/licenses/LICENSE-2.0
+##############################################################################
+
+PROMISE_BRANCH=master
+PROMISE_REPO=https://github.com/opnfv/fuel-plugin-promise.git
+PROMISE_CHANGE=
