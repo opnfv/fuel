81ed415e34a11fc828d6af3c6b780029fa2edc47
Jonas Bjurel <jonas.bjurel@ericsson.com>
2016-02-02 23:52:10 +0100

    Correcting license and copyright terms throughout the repo
    Please review this carefully!
    
    Strategy:
    1) No strict research on copyright/who did what (if you want it changed
       contact the Fuel team or Jonas Bjurel)
    2) Licence statements will not be added to the following file types:
       - Patch files or orig files for patches
       - Config files generated by ordinary tools, like libvirt/visrsh
       - GIT dierctive file
       - Other auto-generated files
    
    Change-Id: I48504c6f27925445dc44683a27a575bcab78d828
    Signed-off-by: Jonas Bjurel <jonas.bjurel@ericsson.com>
    (cherry picked from commit f9e50fa686599ff4a7e94805279820d3cdfefbc4)

--- build/docker/ubuntu-builder/install_docker.sh
+++ build/docker/ubuntu-builder/install_docker.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
+##############################################################################
+# Copyright (c) 2015 Ericsson AB and others.
+# stefan.k.berg@ericsson.com
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the Apache License, Version 2.0
+# which accompanies this distribution, and is available at
+# http://www.apache.org/licenses/LICENSE-2.0
+##############################################################################
+
 # From prepare-build-env.sh of Fuel
 # Check if docker is installed
 if hash docker 2>/dev/null; then
