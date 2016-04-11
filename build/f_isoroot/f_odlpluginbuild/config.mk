18b3dd672e6aae600abe0a95f2d60fae0281d3e6
Nikolas Hermanns <nikolas.hermanns@ericsson.com>
2016-04-11 14:46:05 +0200

    Redo config of plugin build
    - Split up config into a new config file
    - add revision to better point to a branch
      instrad of a revision.
    
    Change-Id: I341138973b56fb0807478ee08cab039ab31d6503

---
+++ build/f_isoroot/f_odlpluginbuild/config.mk
@@ -0,0 +1,17 @@
+##############################################################################
+# Copyright (c) 2015 Ericsson AB and others.
+# jonas.bjurel@eicsson.com
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the Apache License, Version 2.0
+# which accompanies this distribution, and is available at
+# http://www.apache.org/licenses/LICENSE-2.0
+##############################################################################
+
+ODL_BRANCH=brahmaputra-sr2
+ODL_CHANGE=b876df2345bf8c785dceea74f4c271fdfcc9563c
+ODL_REPO="https://github.com/openstack/fuel-plugin-opendaylight.git"
+
+export USE_JAVA8=true
+export JAVA8_URL=https://launchpad.net/~openjdk-r/+archive/ubuntu/ppa/+files/openjdk-8-jre-headless_8u72-b15-1~trusty1_amd64.deb
+export ODL_TARBALL_LOCATION=https://nexus.opendaylight.org/content/repositories/opendaylight.release/org/opendaylight/integration/distribution-karaf/0.4.1-Beryllium-SR1/distribution-karaf-0.4.1-Beryllium-SR1.tar.gz
+export ODL_VERSION_NUMBER=0.4.1
