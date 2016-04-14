679a3bb89d529444e5db50e261fb9497b6fa2f72
ferenc Cserepkei <ferenc.cserepkei@ericsson.com>
2016-04-14 18:06:56 +0200

    Add experimental odl
    - sfc odl feature set updated
    - odl build with netvirt yang load fix
    
    Change-Id: I7161a22325cd4577daf7db11f2fd3b583bb35b73
    Signed-off-by: ferenc Cserepkei <ferenc.cserepkei@ericsson.com>

--- build/f_isoroot/f_odlpluginbuild/config.mk
+++ build/f_isoroot/f_odlpluginbuild/config.mk
@@ -7,11 +7,11 @@
 # http://www.apache.org/licenses/LICENSE-2.0
 ##############################################################################
 
-ODL_BRANCH=brahmaputra-sr2
-ODL_CHANGE=280810a01c2a05535272df86dc599ddacd23bb78
+ODL_BRANCH=master
+ODL_CHANGE=c3e77f6abf90cbbc41e059a8eee19bd55098579d
 ODL_REPO="https://github.com/openstack/fuel-plugin-opendaylight.git"
 
 export USE_JAVA8=true
 export JAVA8_URL=https://launchpad.net/~openjdk-r/+archive/ubuntu/ppa/+files/openjdk-8-jre-headless_8u72-b15-1~trusty1_amd64.deb
-export ODL_TARBALL_LOCATION=https://nexus.opendaylight.org/content/repositories/opendaylight.release/org/opendaylight/integration/distribution-karaf/0.4.1-Beryllium-SR1/distribution-karaf-0.4.1-Beryllium-SR1.tar.gz
-export ODL_VERSION_NUMBER=0.4.1
+export ODL_TARBALL_LOCATION=https://nexus.opendaylight.org/service/local/repositories/opendaylight.snapshot/content/org/opendaylight/integration/distribution-karaf/0.4.2-SNAPSHOT/distribution-karaf-0.4.2-20160414.163022-222.tar.gz
+export ODL_VERSION_NUMBER=0.4.2
