2dcfdda52119ac098dca01af6912be353c3436fb
ferenc Cserepkei <ferenc.cserepkei@ericsson.com>
2016-04-18 08:16:54 +0200

    Add experimental odl
    To have netvirt yang load namespace problem fixed in odl
     - Taken notice of Michal Skalski's comments
     - Tim Rozet's recommendation for ODL nexus URL is used
    
    Change-Id: I3e5ceb5833516baef40110b3498393ca6c93cacb
    Signed-off-by: ferenc Cserepkei <ferenc.cserepkei@ericsson.com>

--- build/f_isoroot/f_odlpluginbuild/config.mk
+++ build/f_isoroot/f_odlpluginbuild/config.mk
@@ -7,11 +7,11 @@
 # http://www.apache.org/licenses/LICENSE-2.0
 ##############################################################################
 
-ODL_BRANCH=brahmaputra-sr2
-ODL_CHANGE=ec9474c1178e7451ec75f6e306ac84424a84a28a
+ODL_BRANCH=master
+ODL_CHANGE=c3e77f6abf90cbbc41e059a8eee19bd55098579d
 ODL_REPO="https://github.com/openstack/fuel-plugin-opendaylight.git"
 
 export USE_JAVA8=true
 export JAVA8_URL=https://launchpad.net/~openjdk-r/+archive/ubuntu/ppa/+files/openjdk-8-jre-headless_8u72-b15-1~trusty1_amd64.deb
-export ODL_TARBALL_LOCATION=https://nexus.opendaylight.org/content/repositories/opendaylight.release/org/opendaylight/integration/distribution-karaf/0.4.1-Beryllium-SR1/distribution-karaf-0.4.1-Beryllium-SR1.tar.gz
-export ODL_VERSION_NUMBER=0.4.1
+export ODL_TARBALL_LOCATION=https://nexus.opendaylight.org/content/repositories/staging/org/opendaylight/integration/distribution-karaf/0.4.2-Beryllium-SR2/distribution-karaf-0.4.2-Beryllium-SR2.tar.gz
+export ODL_VERSION_NUMBER=0.4.2
