b16f559a30ce28a6b528e4eab734cb5ea0ef23fc
Michal Skalski <mskalski@mirantis.com>
2016-05-19 16:52:18 +0200

    Build odl plugin for mitaka
    Change-Id: I59b96a424a753f880b4ac16abd806851ad3f9533
    Signed-off-by: Michal Skalski <mskalski@mirantis.com>

--- build/f_isoroot/f_odlpluginbuild/config.mk
+++ build/f_isoroot/f_odlpluginbuild/config.mk
@@ -7,11 +7,15 @@
 # http://www.apache.org/licenses/LICENSE-2.0
 ##############################################################################
 
-ODL_BRANCH=brahmaputra-sr2
-ODL_CHANGE=ec9474c1178e7451ec75f6e306ac84424a84a28a
-ODL_REPO="https://github.com/openstack/fuel-plugin-opendaylight.git"
+ODL_BRANCH ?= master
+ODL_CHANGE ?= 1c8443ffc64af120337740551307378d1c21535d
+ODL_REPO ?= https://github.com/openstack/fuel-plugin-opendaylight.git
+
+FPB_BRANCH ?= master
+FPB_CHANGE ?= 82191ca16b40021e445e854fad37c65cd8e70b0c
+FPB_REPO ?= https://github.com/openstack/fuel-plugins
 
 export USE_JAVA8=true
 export JAVA8_URL=https://launchpad.net/~openjdk-r/+archive/ubuntu/ppa/+files/openjdk-8-jre-headless_8u72-b15-1~trusty1_amd64.deb
-export ODL_TARBALL_LOCATION=https://nexus.opendaylight.org/content/repositories/opendaylight.release/org/opendaylight/integration/distribution-karaf/0.4.1-Beryllium-SR1/distribution-karaf-0.4.1-Beryllium-SR1.tar.gz
-export ODL_VERSION_NUMBER=0.4.1
+export ODL_TARBALL_LOCATION=https://nexus.opendaylight.org/content/groups/public/org/opendaylight/integration/distribution-karaf/0.4.2-Beryllium-SR2/distribution-karaf-0.4.2-Beryllium-SR2.tar.gz
+export ODL_VERSION_NUMBER=0.4.2
