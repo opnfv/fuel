21cd743d41da5cbd2f6beef4959f5bddd495e717
Guo Ruijing <ruijing.guo@intel.com>
2016-06-20 17:09:51 -0700

    Add fuel yardstick plugin
    Change-Id: Ie6a980ad23e43f6eef775c74d005358d4ab859cc
    Signed-off-by: Guo Ruijing <ruijing.guo@intel.com>

---
+++ build/f_isoroot/f_yardstick-pluginbuild/config.mk
@@ -0,0 +1,12 @@
+##############################################################################
+# Copyright (c) 2015 Ericsson AB and others.
+# jonas.bjurel@eicsson.com
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the Apache License, Version 2.0
+# which accompanies this distribution, and is available at
+# http://www.apache.org/licenses/LICENSE-2.0
+##############################################################################
+
+YARDSTICK_BRANCH=675eef731a098f83aa98c2021b151b7e5ba1da7d
+YARDSTICK_REPO=https://gerrit.opnfv.org/gerrit/yardstick.git
+YARDSTICK_CHANGE=
