65202bdc6d072460257698c92eb4b88731bb0c20
Guo Ruijing <ruijing.guo@intel.com>
2016-06-21 22:30:09 -0700

    Add fuel@opnfv build based on vagrant
    Change-Id: I5d180f060f27d49b03d6b2eda4750845a3490348
    Signed-off-by: Guo Ruijing <ruijing.guo@intel.com>

---
+++ build/vagrant/build_fuel.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+sudo apt-get update
+sudo apt-get install -y vagrant virtualbox
+vagrant destroy -f; vagrant up
+vagrant ssh -c "sudo apt-get update; sudo apt-get install -y linux-generic-lts-vivid"
+vagrant reload
+vagrant ssh -c "/vagrant/setup_fuel.sh"
+vagrant ssh -c "cd fuel/build; make"
