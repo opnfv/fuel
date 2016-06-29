b24f6facf80663041dc1c0d298e9f01fdd81f93e
Michal Skalski <mskalski@mirantis.com>
2016-06-29 09:37:37 +0200

    Use openjdk-8-jre by default
    Package openjdk-8-jre-headless is now a part of mos repositories,
    opendaylight depends on it now.
    
    Change-Id: I27bf15d9e8e4361691c742e71a3f8293f0359f70
    Signed-off-by: Michal Skalski <mskalski@mirantis.com>

--- build/f_isoroot/f_odlpluginbuild/config.mk
+++ build/f_isoroot/f_odlpluginbuild/config.mk
@@ -8,14 +8,12 @@
 ##############################################################################
 
 ODL_BRANCH ?= master
-ODL_CHANGE ?= 1c8443ffc64af120337740551307378d1c21535d
+ODL_CHANGE ?= 8eb70403e009f1a1f5ea3d2a027165139029d597
 ODL_REPO ?= https://github.com/openstack/fuel-plugin-opendaylight.git
 
 FPB_BRANCH ?= master
 FPB_CHANGE ?= 82191ca16b40021e445e854fad37c65cd8e70b0c
 FPB_REPO ?= https://github.com/openstack/fuel-plugins
 
-export USE_JAVA8?=true
-export JAVA8_URL?=https://launchpad.net/~openjdk-r/+archive/ubuntu/ppa/+files/openjdk-8-jre-headless_8u72-b15-1~trusty1_amd64.deb
 export ODL_TARBALL_LOCATION?=https://nexus.opendaylight.org/content/groups/public/org/opendaylight/integration/distribution-karaf/0.4.2-Beryllium-SR2/distribution-karaf-0.4.2-Beryllium-SR2.tar.gz
 export ODL_VERSION_NUMBER?=0.4.2
