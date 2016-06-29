6ca5b5ff78ee4825c3825825565b194c8fec85ab
Guo Ruijing <ruijing.guo@intel.com>
2016-06-29 14:24:25 +0800

    Fix NSH patches
    Change-Id: I04d03ce0b803aa23575dd2640e33eafa00f948bc
    Signed-off-by: Guo Ruijing <ruijing.guo@intel.com>

--- build/f_isoroot/f_ovs-nsh-dpdk-pluginbuild/config.mk
+++ build/f_isoroot/f_ovs-nsh-dpdk-pluginbuild/config.mk
@@ -7,6 +7,6 @@
 # http://www.apache.org/licenses/LICENSE-2.0
 ##############################################################################
 
-OVS_NSH_DPDK_BRANCH=fd77b5d48782e67306c3763dc1908f0fb342c51d
+OVS_NSH_DPDK_BRANCH=6f5f62221049d130ad190030a36db3147d6deb10
 OVS_NSH_DPDK_REPO=https://review.openstack.org/openstack/fuel-plugin-ovs
 OVS_NSH_DPDK_CHANGE=
