d1466b0f8792bde0a1e39d2071a03d0317e0f7ee
Michal Skalski <mskalski@mirantis.com>
2016-07-05 12:21:42 +0200

    Fix ODL deployments
    Change-Id: Idae02b340d3a64109deeda7ef67574430b928219
    Signed-off-by: Michal Skalski <mskalski@mirantis.com>

--- build/f_isoroot/f_odlpluginbuild/config.mk
+++ build/f_isoroot/f_odlpluginbuild/config.mk
@@ -8,12 +8,8 @@
 ##############################################################################
 
 ODL_BRANCH ?= master
-ODL_CHANGE ?= c539dc87d905e9c9992d6624c11814c4eda4c5ca
+ODL_CHANGE ?= 32b3824f8437588b0b6baacc36291670c2d692c6
 ODL_REPO ?= https://github.com/openstack/fuel-plugin-opendaylight.git
-
-FPB_BRANCH ?= master
-FPB_CHANGE ?= 82191ca16b40021e445e854fad37c65cd8e70b0c
-FPB_REPO ?= https://github.com/openstack/fuel-plugins
 
 export ODL_TARBALL_LOCATION?=https://nexus.opendaylight.org/content/groups/public/org/opendaylight/integration/distribution-karaf/0.4.2-Beryllium-SR2/distribution-karaf-0.4.2-Beryllium-SR2.tar.gz
 export ODL_VERSION_NUMBER?=0.4.2
