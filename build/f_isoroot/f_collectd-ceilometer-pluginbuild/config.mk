50b6b5f994a96ca726d4cb34c714f778a99fc3c8
Guo Ruijing <ruijing.guo@intel.com>
2016-07-13 01:32:42 +0800

    Add fuel collectd ceilometer plugin
    Change-Id: I2f95d9166e2d0d7508551de8b87dc6c84800e3c1
    Signed-off-by: Guo Ruijing <ruijing.guo@intel.com>

---
+++ build/f_isoroot/f_collectd-ceilometer-pluginbuild/config.mk
@@ -0,0 +1,13 @@
+##############################################################################
+# Copyright (c) 2016 Ericsson AB and others.
+# jonas.bjurel@eicsson.com
+# ruijing.guo@intel.com
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the Apache License, Version 2.0
+# which accompanies this distribution, and is available at
+# http://www.apache.org/licenses/LICENSE-2.0
+##############################################################################
+
+COLLECTD_CEILOMETER_BRANCH?=6a0837238ff13d3294e5a7181fc3de40e3094625
+COLLECTD_CEILOMETER_REPO?=http://gerrit.opnfv.org/gerrit/fastpathmetrics
+COLLECTD_CEILOMETER_CHANGE?=
