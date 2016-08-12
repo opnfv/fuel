c8efdda84bd1e4c2c40eb3ae960d348cea892e23
dongwenjuan <dong.wenjuan@zte.com.cn>
2016-08-12 15:44:07 +0800

    add doctor fuel plugin
    JIRA: FUEL-159
    
    Change-Id: If437a262b0457ca9a571e5f5a0858d65bb90a9b2
    Signed-off-by: dongwenjuan <dong.wenjuan@zte.com.cn>

---
+++ build/f_isoroot/f_doctor-pluginbuild/config.mk
@@ -0,0 +1,12 @@
+##############################################################################
+# Copyright (c) 2016 ZTE and others.
+# dong.wenjuan@zte.com.cn
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the Apache License, Version 2.0
+# which accompanies this distribution, and is available at
+# http://www.apache.org/licenses/LICENSE-2.0
+##############################################################################
+
+DOCTOR_PLUGIN_BRANCH?=master
+DOCTOR_PLUGIN_REPO?=https://github.com/openzero-zte/fuel-plugin-doctor
+DOCTOR_PLUGIN_CHANGE?=7e342ac63a6fe71b658537cedae51898787d0430
