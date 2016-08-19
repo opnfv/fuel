8580caa96234cdda3f7c4cf4a5965a0fd411fce0
Michal Skalski <mskalski@mirantis.com>
2016-08-19 20:54:19 +0200

    [KVM]Add support for Broadcom NetXtremeII 10Gb
    This NIC is used on Ericsson CI POD2
    
    JIRA: KVMFORNFV-45
    JIRA: FUEL-171
    
    Change-Id: I44576b399b6195ccc1e64dfc0b04e239ced8acc7
    Signed-off-by: Michal Skalski <mskalski@mirantis.com>

--- build/f_isoroot/f_kvm-pluginbuild/config.mk
+++ build/f_isoroot/f_kvm-pluginbuild/config.mk
@@ -7,6 +7,6 @@
 # http://www.apache.org/licenses/LICENSE-2.0
 ##############################################################################
 
-KVMFORNFV_BRANCH=2aa991a490387f3910a7e1bf02de83a7efb28014
+KVMFORNFV_BRANCH=b1c117f1c8414bddbe4370414590f5f0b62ae4d1
 KVMFORNFV_REPO=https://gerrit.opnfv.org/gerrit/kvmfornfv
 KVMFORNFV_CHANGE=
