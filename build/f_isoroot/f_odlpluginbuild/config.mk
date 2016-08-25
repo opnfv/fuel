f7c948fa78ec487723ea6328bb9d145293057e8d
Michal Skalski <mskalski@mirantis.com>
2016-08-25 10:18:21 +0200

    [ODL] Fix odl deployment on separate node
    JIRA: FUEL-185
    
    Change-Id: Id14d30154b63abc915a0cd55835bb72cee862dde
    Signed-off-by: Michal Skalski <mskalski@mirantis.com>
    (cherry picked from commit ba1cb485398d811d2f473406ae085993bfd9f68a)

--- build/f_isoroot/f_odlpluginbuild/config.mk
+++ build/f_isoroot/f_odlpluginbuild/config.mk
@@ -8,7 +8,7 @@
 ##############################################################################
 
 FUEL_PLUGIN_ODL_BRANCH ?= master
-FUEL_PLUGIN_ODL_CHANGE ?= 8cc22c4717d2da338135d854e1d13aac3ef75314
+FUEL_PLUGIN_ODL_CHANGE ?= 0ff4ebdbfe5f681f0b44eea14c9c2b3dbb10b1d5
 FUEL_PLUGIN_ODL_REPO ?= https://github.com/openstack/fuel-plugin-opendaylight.git
 
 export ODL_TARBALL_LOCATION?=https://nexus.opendaylight.org/content/repositories/public/org/opendaylight/integration/distribution-karaf/0.4.3-Beryllium-SR3/distribution-karaf-0.4.3-Beryllium-SR3.tar.gz
