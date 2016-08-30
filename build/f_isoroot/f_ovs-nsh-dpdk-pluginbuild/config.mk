74dfe010283e5458d1e3a71dbdcf8f09cfd1c8b9
Michael Polenchuk <mpolenchuk@mirantis.com>
2016-08-30 10:46:30 +0300

    [OVS-DPDK] Allocate dpdk socket memory
    JIRA: FUEL-186
    Change-Id: Id44a786f69d78b378fbabea2c70ab1ca78651729
    Signed-off-by: Michael Polenchuk <mpolenchuk@mirantis.com>

--- build/f_isoroot/f_ovs-nsh-dpdk-pluginbuild/config.mk
+++ build/f_isoroot/f_ovs-nsh-dpdk-pluginbuild/config.mk
@@ -7,6 +7,6 @@
 # http://www.apache.org/licenses/LICENSE-2.0
 ##############################################################################
 
-OVS_NSH_DPDK_BRANCH?=2537604061e8f44f45f0fa5c187f448534bf59ca
+OVS_NSH_DPDK_BRANCH?=7d02f4c328e1f712bb2bb3eee0d77593f22b2fba
 OVS_NSH_DPDK_REPO?=https://review.openstack.org/openstack/fuel-plugin-ovs
 OVS_NSH_DPDK_CHANGE?=
