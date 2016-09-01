5589b0e1659e8e0de0bc1055753685c9fd58f13a
Michael Polenchuk <mpolenchuk@mirantis.com>
2016-09-01 16:23:10 +0300

    [OVS-DPDK] Set default for dpdk_socket_mem
    Change-Id: I175178cdcbab52ac024157d2c5707fd7e1236dd4
    Signed-off-by: Michael Polenchuk <mpolenchuk@mirantis.com>

--- build/f_isoroot/f_ovs-nsh-dpdk-pluginbuild/config.mk
+++ build/f_isoroot/f_ovs-nsh-dpdk-pluginbuild/config.mk
@@ -7,6 +7,6 @@
 # http://www.apache.org/licenses/LICENSE-2.0
 ##############################################################################
 
-OVS_NSH_DPDK_BRANCH?=7d02f4c328e1f712bb2bb3eee0d77593f22b2fba
+OVS_NSH_DPDK_BRANCH?=137748b828f03debe5ae1b40a4ee440b8f5e96f8
 OVS_NSH_DPDK_REPO?=https://review.openstack.org/openstack/fuel-plugin-ovs
 OVS_NSH_DPDK_CHANGE?=
