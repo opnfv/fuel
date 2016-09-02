491c567c31839c0ea06b0b9e57d7bc553f92a494
Michael Polenchuk <mpolenchuk@mirantis.com>
2016-09-02 10:15:27 +0300

    [OVS-DPDK] Allocate dpdk socket memory
    Change-Id: Ic84094e9842295df936d9f87d0777009e0020013
    Signed-off-by: Michael Polenchuk <mpolenchuk@mirantis.com>

--- build/f_isoroot/f_ovs-nsh-dpdk-pluginbuild/config.mk
+++ build/f_isoroot/f_ovs-nsh-dpdk-pluginbuild/config.mk
@@ -7,6 +7,6 @@
 # http://www.apache.org/licenses/LICENSE-2.0
 ##############################################################################
 
-OVS_NSH_DPDK_BRANCH?=2537604061e8f44f45f0fa5c187f448534bf59ca
+OVS_NSH_DPDK_BRANCH?=137748b828f03debe5ae1b40a4ee440b8f5e96f8
 OVS_NSH_DPDK_REPO?=https://review.openstack.org/openstack/fuel-plugin-ovs
 OVS_NSH_DPDK_CHANGE?=
