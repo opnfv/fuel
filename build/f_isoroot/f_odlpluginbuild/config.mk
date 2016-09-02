aa4ee71f944021d0be6f1511ca0d480f043de866
Michal Skalski <mskalski@mirantis.com>
2016-09-02 12:23:20 +0200

    Change source of ODL Boron tarball
    For SFC scenario we need to use ODL Boron. Since it is not yet released
    there is no persistent location for a tarball. Use opnfv artifacts to
    store Boron build which include required changes.
    
    Change-Id: I43e35dbdf5ca0160d48a1adc817db071fd5db85d
    Signed-off-by: Michal Skalski <mskalski@mirantis.com>

--- build/f_isoroot/f_odlpluginbuild/config.mk
+++ build/f_isoroot/f_odlpluginbuild/config.mk
@@ -13,4 +13,4 @@ FUEL_PLUGIN_ODL_REPO ?= https://github.com/openstack/fuel-plugin-opendaylight.gi
 
 export ODL_TARBALL_LOCATION?=https://nexus.opendaylight.org/content/repositories/public/org/opendaylight/integration/distribution-karaf/0.4.3-Beryllium-SR3/distribution-karaf-0.4.3-Beryllium-SR3.tar.gz
 export ODL_VERSION_NUMBER?=0.4.3
-export ODL_BORON_TARBALL_LOCATION?=https://nexus.opendaylight.org/content/repositories/staging/org/opendaylight/integration/distribution-karaf/0.5.0-Boron/distribution-karaf-0.5.0-Boron.tar.gz
+export ODL_BORON_TARBALL_LOCATION?=http://artifacts.opnfv.org/fuel/colorado/distribution-karaf-0.5.0-Boron-RC2.tar.gz
