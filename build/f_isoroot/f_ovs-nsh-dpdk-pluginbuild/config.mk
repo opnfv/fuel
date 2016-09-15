b90311bf3240c0f57ee747e6cf3e57e07a6cf5fa
billyom <billy.o.mahony@intel.com>
2016-09-15 13:27:50 +0100

    Uplift fuel-plugin-ovs
    Pull in "Update flavors mem_page_size"
    
    Change-Id: I3e285cefee561dbbc02eb9ab7d2e9c01eef2742a
    Signed-off-by: Jonas Bjurel <jonas.bjurel@ericsson.com>

--- build/f_isoroot/f_ovs-nsh-dpdk-pluginbuild/config.mk
+++ build/f_isoroot/f_ovs-nsh-dpdk-pluginbuild/config.mk
@@ -7,6 +7,6 @@
 # http://www.apache.org/licenses/LICENSE-2.0
 ##############################################################################
 
-OVS_NSH_DPDK_BRANCH?=c77582ea96880cc854b9d82cbaa5b5ed2c5c4639
+OVS_NSH_DPDK_BRANCH?=42160542e6e9151f5f44b738d8d76e88911cd07f
 OVS_NSH_DPDK_REPO?=https://review.openstack.org/openstack/fuel-plugin-ovs
 OVS_NSH_DPDK_CHANGE?=
