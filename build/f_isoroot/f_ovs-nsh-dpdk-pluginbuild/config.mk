861836e6bbd545d3dcee1bd041ff867fb4e3d357
Michael Polenchuk <mpolenchuk@mirantis.com>
2016-09-15 17:36:58 +0300

    [OVS-DPDK] Turn off missing options
    Get all gcc/compile options to conform to corei7 arch.
    
    JIRA: FUEL-186
    Change-Id: I5ce76631aac9174c69362f96f71e26285fc95842
    Signed-off-by: Michael Polenchuk <mpolenchuk@mirantis.com>

--- build/f_isoroot/f_ovs-nsh-dpdk-pluginbuild/config.mk
+++ build/f_isoroot/f_ovs-nsh-dpdk-pluginbuild/config.mk
@@ -7,6 +7,6 @@
 # http://www.apache.org/licenses/LICENSE-2.0
 ##############################################################################
 
-OVS_NSH_DPDK_BRANCH?=42160542e6e9151f5f44b738d8d76e88911cd07f
+OVS_NSH_DPDK_BRANCH?=074aadce66ef3cf322a00a3cff5c57deb84088b5
 OVS_NSH_DPDK_REPO?=https://review.openstack.org/openstack/fuel-plugin-ovs
 OVS_NSH_DPDK_CHANGE?=
