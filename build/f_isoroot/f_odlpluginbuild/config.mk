526e7d2d4678015230afd77c9b36e5d6cf1ec7d0
Michal Skalski <mskalski@mirantis.com>
2016-10-06 15:12:34 +0200

    [ODL] Use OpenDaylight Boron
    ODL Boron will be used in all scenarios except bgpvpn beacuse vpnservice
    was removed in this release, we still will use Beryllium for this
    scenario.
    This change only replace Beryllium with Boron, it does not use any new
    features like odl-netvirt-openstack.
    
    Change-Id: I95264cc97f5b9e922ab3fb7dc024724d9fb83e9c
    Signed-off-by: Michal Skalski <mskalski@mirantis.com>

--- build/f_isoroot/f_odlpluginbuild/config.mk
+++ build/f_isoroot/f_odlpluginbuild/config.mk
@@ -8,9 +8,8 @@
 ##############################################################################
 
 FUEL_PLUGIN_ODL_BRANCH ?= master
-FUEL_PLUGIN_ODL_CHANGE ?= 0ff4ebdbfe5f681f0b44eea14c9c2b3dbb10b1d5
+FUEL_PLUGIN_ODL_CHANGE ?= 7281b0db9171e5aeda0c23ba655f086a909395a1
 FUEL_PLUGIN_ODL_REPO ?= https://github.com/openstack/fuel-plugin-opendaylight.git
 
-export ODL_TARBALL_LOCATION?=https://nexus.opendaylight.org/content/repositories/public/org/opendaylight/integration/distribution-karaf/0.4.3-Beryllium-SR3/distribution-karaf-0.4.3-Beryllium-SR3.tar.gz
-export ODL_VERSION_NUMBER?=0.4.3
-export ODL_BORON_TARBALL_LOCATION?=http://artifacts.opnfv.org/fuel/colorado/vendor/distribution-karaf-0.5.0-Boron-RC3.5.tar.gz
+export ODL_TARBALL_LOCATION?=https://nexus.opendaylight.org/content/groups/public/org/opendaylight/integration/distribution-karaf/0.5.0-Boron/distribution-karaf-0.5.0-Boron.tar.gz
+export ODL_VERSION_NUMBER?=0.5.0
