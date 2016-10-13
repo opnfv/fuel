a68c2e41d8289d6dbfce44827cf94872d6247cb1
Michal Skalski <mskalski@mirantis.com>
2016-10-13 13:53:25 +0200

    [WIP] Uplift to Fuel 9.1
    Change-Id: Ia4c6a9b77acb8415ad879fe3f78b01201527331c
    Signed-off-by: Michal Skalski <mskalski@mirantis.com>

--- build/config.mk
+++ build/config.mk
@@ -9,24 +9,28 @@
 ##############################################################################
 
 FUEL_MAIN_REPO := https://github.com/openstack/fuel-main
-FUEL_MAIN_TAG := 9.0.1
+FUEL_MAIN_TAG := 9.1
 MOS_VERSION = 9.0
 OPENSTACK_VERSION = mitaka-9.0
 
-# Pinning down exact Fuel repo versions for Fuel 9.0.1
-export FUELLIB_COMMIT?=e283b62750d9e26355981b3ad3be7c880944ae0f
-export NAILGUN_COMMIT?=e2b85bafb68c348f25cb7cceda81edc668ba2e64
-export PYTHON_FUELCLIENT_COMMIT?=67d8c693a670d27c239d5d175f3ea2a0512c498c
-export FUEL_AGENT_COMMIT?=7ffbf39caf5845bd82b8ce20a7766cf24aa803fb
-export FUEL_NAILGUN_AGENT_COMMIT?=46fa0db0f8944f9e67699d281d462678aaf4db26
-export ASTUTE_COMMIT?=390b257240d49cc5e94ed5c4fcd940b5f2f6ec64
-export OSTF_COMMIT?=f09c98ff7cc71ee612b2450f68a19f2f9c64345a
+# Pinning down exact Fuel repo versions for Fuel 9.1
+export FUELLIB_COMMIT?=c6103ad39257e89fa5fae59bf4bfb54ffae07c2b
+export NAILGUN_COMMIT?=78fbccc9ac1fad1f05bfc9e68677d4ef24b1a0aa
+# Top of stable/mitaka
+export PYTHON_FUELCLIENT_COMMIT?=81d1905c00d3c4522f08849a39b2c5a730bb1093
+
+export FUEL_AGENT_COMMIT?=898bcca75224ad82fa98a85b77651faaf554e2b6
+export FUEL_NAILGUN_AGENT_COMMIT?=e7bd486ec6d8c65ce5611dad69059e0b7c22f394
+export ASTUTE_COMMIT?=2158ea9a60c3f750ec32fd3911213683dadad21f
+export OSTF_COMMIT?=ff066dd2900dbc3ec50764b4aa86136098f5c0f3
 export FUEL_MIRROR_COMMIT?=d1ef06b530ce2149230953bb3810a88ecaff870c
-export FUELMENU_COMMIT?=0ed9e206ed1c6271121d3acf52a6bf757411286b
+export FUELMENU_COMMIT?=c4ff3eebde75daab40fb7c7aba175543024e9fb6
 export SHOTGUN_COMMIT?=781a8cfa0b6eb290e730429fe2792f2b6f5e0c11
-export NETWORKCHECKER_COMMIT?=fcb47dd095a76288aacf924de574e39709e1f3ca
+# Top of stable/mitaka
+export NETWORKCHECKER_COMMIT?=2dc38414ab9bc32e9dc5b1ab71bea4e4d8eb1bb3
+
 export FUELUPGRADE_COMMIT?=c1c4bac6a467145ac4fac73e4a7dd2b00380ecfb
-export FUEL_UI_COMMIT?=90de7ef4477230cb7335453ed26ed4306ca6f04f
+export FUEL_UI_COMMIT?=b0dc8f854141ba562f2e24374cb3f1db2edbb78b
 
 # for the patches applying purposes
 export GIT_COMMITTER_NAME?=Fuel OPNFV
