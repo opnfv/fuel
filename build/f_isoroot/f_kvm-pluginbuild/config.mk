b3d6a6c07169fc92e5931418182fdd3be14a81d5
Michael Polenchuk <mpolenchuk@mirantis.com>
2016-11-18 12:45:37 +0300

    Turn kvm-plugin back on
    Change-Id: I7c46f263ec584b09b6022d9e961ae3c44c642954
    Signed-off-by: Michael Polenchuk <mpolenchuk@mirantis.com>

--- build/f_isoroot/f_kvm-pluginbuild/config.mk
+++ build/f_isoroot/f_kvm-pluginbuild/config.mk
@@ -7,6 +7,6 @@
 # http://www.apache.org/licenses/LICENSE-2.0
 ##############################################################################
 
-KVMFORNFV_BRANCH=stable/colorado
+KVMFORNFV_BRANCH=master
 KVMFORNFV_REPO=https://gerrit.opnfv.org/gerrit/kvmfornfv
-KVMFORNFV_CHANGE=e6588d1d59d7ac24f25f644650e6845561dd441b
+KVMFORNFV_CHANGE=74248cb2594babbb144e191a3fbc755200aea043
