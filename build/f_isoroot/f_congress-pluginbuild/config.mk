9e5b5a242757478f93a4018bd25a465f4884d21f
Fedor Zhadaev <fzhadaev@mirantis.com>
2016-12-27 12:18:25 +0400

    Add building of congress plugin
    JIRA: FUEL-230
    
    Change-Id: I315a9ed955cafa68a2454c30daac9dbf41f76e05
    Signed-off-by: Fedor Zhadaev <fzhadaev@mirantis.com>

---
+++ build/f_isoroot/f_congress-pluginbuild/config.mk
@@ -0,0 +1,13 @@
+##############################################################################
+# Copyright (c) 2016 Mirantis Inc and others.
+# fzhadaev@mirantis.com
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the Apache License, Version 2.0
+# which accompanies this distribution, and is available at
+# http://www.apache.org/licenses/LICENSE-2.0
+##############################################################################
+
+CONGRESS_BRANCH?=master
+# TODO(fzhadaev): change to openstack/fuel-plugin-congress
+CONGRESS_REPO?="https://github.com/fzhadaev/fuel-plugin-congress"
+CONGRESS_CHANGE?=951a5ef26af16215b4bb7730c55c3c44fadb7cee
