02fb19bb23fde65f048ee5b747a6c42cb800d0b1
Edwin Zhai <edwin.zhai@intel.com>
2016-12-28 14:24:15 -0800

    Minor fix for yardstick plugin
    Fix installation dependency and authentication failure.
    
    Change-Id: Ib15ecccaefd1e0214edd2e1d1f52ab3f9ed99f3a
    Signed-off-by: Edwin Zhai <edwin.zhai@intel.com>

--- build/f_isoroot/f_yardstick-pluginbuild/config.mk
+++ build/f_isoroot/f_yardstick-pluginbuild/config.mk
@@ -8,6 +8,6 @@
 # http://www.apache.org/licenses/LICENSE-2.0
 ##############################################################################
 
-YARDSTICK_BRANCH?=0e23c697e6329a57ba168cc57886b436ea87cdc4
+YARDSTICK_BRANCH?=31f563b8aa2e6dcf652cb7e9dfdc3f6c7316dc1d
 YARDSTICK_REPO?=https://gerrit.opnfv.org/gerrit/yardstick.git
 YARDSTICK_CHANGE?=
