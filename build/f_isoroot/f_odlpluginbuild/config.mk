e2378211a09397ab35e7c18d53d407360eaf470b
Michael Polenchuk <mpolenchuk@mirantis.com>
2017-02-08 11:26:36 +0400

    [odl] Mend standalone mode condition
    Change-Id: If51ee19a0cd0cb0a83dcf40fda46d0aaa817408e
    Signed-off-by: Michael Polenchuk <mpolenchuk@mirantis.com>

--- build/f_isoroot/f_odlpluginbuild/config.mk
+++ build/f_isoroot/f_odlpluginbuild/config.mk
@@ -8,7 +8,7 @@
 ##############################################################################
 
 FUEL_PLUGIN_ODL_BRANCH ?= master
-FUEL_PLUGIN_ODL_CHANGE ?= 001dc03a0691bb3c94570d447584eea7e12264dc
+FUEL_PLUGIN_ODL_CHANGE ?= 6aa47a6ad6f45175ce9f645d1ac171e0129507b2
 FUEL_PLUGIN_ODL_REPO ?= https://github.com/openstack/fuel-plugin-opendaylight.git
 
 export OPNFV_BUILD?=true
