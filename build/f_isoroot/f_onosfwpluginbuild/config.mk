f6bf9c6e36ea0b095eac8469e9f946712a651139
rssaravana <saravanakumar.somaskandan@huawei.com>
2017-03-16 11:47:50 +0530

    Update for fuel10 onos plugin
    Change-Id: Ie7e9b5c16505223616bc6d796ce8de60236d098c
    Signed-off-by: rssaravana <saravanakumar.somaskandan@huawei.com>

--- build/f_isoroot/f_onosfwpluginbuild/config.mk
+++ build/f_isoroot/f_onosfwpluginbuild/config.mk
@@ -7,5 +7,5 @@
 # http://www.apache.org/licenses/LICENSE-2.0
 ##############################################################################
 
-ONOS_BRANCH=Mitaka
+ONOS_BRANCH=master
 ONOS_REPO=git://git.openstack.org/openstack/fuel-plugin-onos
