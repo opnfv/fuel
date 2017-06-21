5cb761a3ea315ec3d1a9133f22269d694a515b98
Michael Polenchuk <mpolenchuk@mirantis.com>
2017-06-21 14:43:40 +0400

    Make virtual cluster as noha w/ one controller
    * remove glusterfs from cluster
    * increase amount of ram on controller
    * bring in one more compute node
    
    Change-Id: I04ccd4cdaf65bb860d6d3b9c012e035f598e44d5
    Signed-off-by: Michael Polenchuk <mpolenchuk@mirantis.com>

--- mcp/reclass/scripts/openstack.sh
+++ mcp/reclass/scripts/openstack.sh
@@ -7,7 +7,7 @@ ssh $SSH_OPTS ubuntu@$SALT_MASTER bash -s << OPENSTACK_INSTALL_END
   sudo -i
 
   salt-call state.apply salt
-  salt '*' state.apply salt
+  salt '*' state.apply salt || salt '*' state.apply salt
 
   salt -C 'I@salt:master' state.sls linux
   salt -C '* and not cfg01*' state.sls linux
@@ -19,12 +19,7 @@ ssh $SSH_OPTS ubuntu@$SALT_MASTER bash -s << OPENSTACK_INSTALL_END
   salt -C 'I@rabbitmq:server' state.sls rabbitmq
   salt -C 'I@rabbitmq:server' cmd.run "rabbitmqctl cluster_status"
 
-  salt -C 'I@glusterfs:server' state.sls glusterfs.server.service
-  salt -C 'I@glusterfs:server' state.sls glusterfs.server.setup -b 1
-  salt -C 'I@glusterfs:server' cmd.run "gluster peer status; gluster volume status" -b 1
-
   salt -C 'I@galera:master' state.sls galera
-  salt -C 'I@galera:slave' state.sls galera
   salt -C 'I@galera:master' mysql.status | grep -A1 wsrep_cluster_size
 
   salt -C 'I@haproxy:proxy' state.sls haproxy
