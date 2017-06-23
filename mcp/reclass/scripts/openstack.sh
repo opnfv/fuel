3c7e4c3773face367cdf0988cccfc2dfd68f55ab
Michael Polenchuk <mpolenchuk@mirantis.com>
2017-06-23 17:44:33 +0400

    Remove redundant services from noha scenario
    Change-Id: I445f6ac802fbf1b382b18bcb4306eb5538cb1867
    Signed-off-by: Michael Polenchuk <mpolenchuk@mirantis.com>

--- mcp/reclass/scripts/openstack.sh
+++ mcp/reclass/scripts/openstack.sh
@@ -14,28 +14,24 @@ ssh $SSH_OPTS ubuntu@$SALT_MASTER bash -s << OPENSTACK_INSTALL_END
 
   salt '*' state.sls ntp
 
-  salt -C 'I@keepalived:cluster' state.sls keepalived -b 1
-
   salt -C 'I@rabbitmq:server' state.sls rabbitmq
-  salt -C 'I@rabbitmq:server' cmd.run "rabbitmqctl cluster_status"
+  salt -C 'I@rabbitmq:server' cmd.run "rabbitmqctl status"
 
-  salt -C 'I@galera:master' state.sls galera
-  salt -C 'I@galera:master' mysql.status | grep -A1 wsrep_cluster_size
+  salt -C 'I@mysql:server' state.sls mysql
 
-  salt -C 'I@haproxy:proxy' state.sls haproxy
   salt -C 'I@memcached:server' state.sls memcached
 
-  salt -C 'I@keystone:server' state.sls keystone.server -b 1
-  salt -C 'I@keystone:server' cmd.run "service apache2 restart"
-  while true; do salt -C 'I@keystone:client' state.sls keystone.client && break; done
-  salt -C 'I@keystone:server' cmd.run ". /root/keystonercv3; openstack user list"
+  salt -C 'I@keystone:server' state.sls keystone.server
+  salt -C 'I@keystone:server' cmd.run "systemctl restart apache2"
+  while true; do salt -C 'I@keystone:server' state.sls keystone.client && break; done
+  salt -C 'I@keystone:server' cmd.run ". /root/keystonercv3; openstack service list"
 
-  salt -C 'I@glance:server' state.sls glance -b 1
-  salt -C 'I@nova:controller' state.sls nova -b 1
-  salt -C 'I@heat:server' state.sls heat -b 1
-  salt -C 'I@cinder:controller' state.sls cinder -b 1
+  salt -C 'I@glance:server' state.sls glance
+  salt -C 'I@nova:controller' state.sls nova
+  salt -C 'I@heat:server' state.sls heat
+  salt -C 'I@cinder:controller' state.sls cinder
 
-  salt -C 'I@neutron:server' state.sls neutron -b 1
+  salt -C 'I@neutron:server' state.sls neutron
   salt -C 'I@neutron:gateway' state.sls neutron
 
   salt -C 'I@nova:compute' state.sls nova
