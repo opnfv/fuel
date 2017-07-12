d676c91a67684a7efb317d58429c49db7c562473
Michael Polenchuk <mpolenchuk@mirantis.com>
2017-07-12 12:29:30 +0400

    Apply reclass patches before salt master init
    Change-Id: I7bb984880e98b7bdec9aa0b3895a3be9fd75cac0
    Signed-off-by: Michael Polenchuk <mpolenchuk@mirantis.com>

--- mcp/patches/patch.sh
+++ mcp/patches/patch.sh
@@ -1,8 +1,7 @@
-#!/bin/sh
+#!/bin/bash
 
 if [ -r "$1" ]; then
   while IFS=': ' read -r p_dest p_file; do
-    patch -d $p_dest -p1 < /root/fuel/mcp/patches/$p_file
+    [[ "${p_dest}" =~ $2 ]] && patch -fd "${p_dest}" -p1 < "/root/fuel/mcp/patches/${p_file}"
   done < $1
 fi
-
