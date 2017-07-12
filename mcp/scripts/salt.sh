80f11d527e7cb6537f03dd940faf2ee08b64a2ff
Michael Polenchuk <mpolenchuk@mirantis.com>
2017-07-12 12:29:30 +0400

    Apply reclass patches before salt master init
    Change-Id: I7bb984880e98b7bdec9aa0b3895a3be9fd75cac0
    Signed-off-by: Michael Polenchuk <mpolenchuk@mirantis.com>

--- mcp/scripts/salt.sh
+++ mcp/scripts/salt.sh
@@ -17,21 +17,20 @@ ssh ${SSH_OPTS} ubuntu@${SALT_MASTER} bash -s << SALT_INSTALL_END
   git clone --depth=1 --recurse-submodules https://git.opnfv.org/fuel
   ln -s /root/fuel/mcp/reclass /srv/salt/reclass
 
+  mkdir -p /usr/share/salt-formulas/reclass
+  cp -r /root/fuel/mcp/metadata/service /usr/share/salt-formulas/reclass
+  cd /srv/salt/reclass/classes/service && ln -s /usr/share/salt-formulas/reclass/service/opendaylight
+  cd /root/fuel/mcp/patches && ./patch.sh patches.list reclass
+
   cd /srv/salt/scripts
   MASTER_HOSTNAME=cfg01.${CLUSTER_DOMAIN} DISTRIB_REVISION=nightly ./salt-master-init.sh
   salt-key -Ay
 
-  cp -r /root/fuel/mcp/metadata/service /usr/share/salt-formulas/reclass
   cp -r /root/fuel/mcp/salt-formulas/* /usr/share/salt-formulas/env
-  cd /srv/salt/reclass/classes/service && ln -s /usr/share/salt-formulas/reclass/service/opendaylight
-
-  cd /root/fuel/mcp/patches && ./patch.sh patches.list
-
-  salt '*' saltutil.refresh_pillar
-  salt '*' saltutil.sync_all
+  cd /root/fuel/mcp/patches && ./patch.sh patches.list formulas
 
   salt-call state.apply salt
-  salt '*' state.apply salt || salt '*' state.apply salt
+  salt '*' state.apply salt | fgrep -q 'No response' && salt '*' state.apply salt
 
   salt -C 'I@salt:master' state.sls linux
   salt -C '* and not cfg01*' state.sls linux
