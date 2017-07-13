956467be603b7acadbc0e1912b84816bcbfd3850
Alexandru Avadanii <Alexandru.Avadanii@enea.com>
2017-07-13 22:07:35 +0200

    f_tackerpluginbuild: Fix obsolete mitaka branch
    Tacker plugin defaults to using the "stable/mitaka" branch for
    the Tacker Horizon dashboard project, which was meanwhile nuked
    upstream.
    Switch to using the "mitaka-eol" tag instead.
    
    Change-Id: Iacec0ebdff7327bca9c84d9433790a04acd2dc4d
    Signed-off-by: Alexandru Avadanii <Alexandru.Avadanii@enea.com>

--- build/f_isoroot/f_tacker-pluginbuild/config.mk
+++ build/f_isoroot/f_tacker-pluginbuild/config.mk
@@ -10,3 +10,6 @@
 TACKER_BRANCH?=master
 TACKER_REPO?="https://github.com/openstack/fuel-plugin-tacker"
 TACKER_CHANGE?=7068a300df0c695fb4589bf504b29cbed970ba58
+
+# Tacker Horizon Dashboard override; default branch (stable/mitaka) was removed
+export TACKER_HORIZON_BRANCH := mitaka-eol
