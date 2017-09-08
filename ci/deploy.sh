ebd7067904b36ae32566da63e433365948a1f973
Alexandru Avadanii <Alexandru.Avadanii@enea.com>
2017-09-08 23:03:56 +0200

    states: Split virtual_control_plane from maas
    We should eventually also support baremetal deploys without a
    virtualized control plane (VCP), so decouple MaaS provisioning
    from VCP provisioning.
    
    While at it, move "wait_for" bash function from maas state to
    common library file, lib.sh.
    
    Change-Id: I32c33135655cb6aceae901a5f92b51265a8c84b4
    Signed-off-by: Alexandru Avadanii <Alexandru.Avadanii@enea.com>

--- ci/deploy.sh
+++ ci/deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# shellcheck disable=SC2034,SC2154,SC1091
+# shellcheck disable=SC2034,SC2154,SC1090,SC1091
 set -ex
 ##############################################################################
 # Copyright (c) 2017 Ericsson AB, Mirantis Inc., Enea AB and others.
