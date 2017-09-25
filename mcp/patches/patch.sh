513b86f1449878ded444e39028e2118c7c04982f
Alexandru Avadanii <Alexandru.Avadanii@enea.com>
2017-09-26 01:16:54 +0200

    patch.sh: Exit on patch apply failure
    Patches that fail to apply are silently skipped. Since retrying is
    not possible, deploy moves on, making the problem hard to observe.
    
    Change-Id: I3fe01cef6b245de53767a660b9b4fdfae0d5d0ff
    Signed-off-by: Alexandru Avadanii <Alexandru.Avadanii@enea.com>

--- mcp/patches/patch.sh
+++ mcp/patches/patch.sh
@@ -3,6 +3,6 @@
 if [ -r "$1" ]; then
   while IFS=': ' read -r p_dest p_file; do
     [[ "${p_dest}" =~ $2 ]] && \
-      patch -fd "${p_dest}" -p1 < "/root/fuel/mcp/patches/${p_file}"
+      patch -fd "${p_dest}" -p1 < "/root/fuel/mcp/patches/${p_file}" || exit 1
   done < "$1"
 fi
