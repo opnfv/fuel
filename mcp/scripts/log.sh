820721e04309d732e58b90b7a7d57db8136784e0
Alexandru Avadanii <Alexandru.Avadanii@enea.com>
2017-10-03 19:30:12 +0200

    ci/deploy.sh: Move global vars to separate file
    log.sh should also work as standalone script, so factor out
    global variables from <ci/deploy.sh> into <mcp/scripts/globals.sh>,
    which should be sourced by log.sh when required variables are not set.
    
    Change-Id: I5a5bb85ee5835f567d6910b11c137d95e2147714
    Signed-off-by: Alexandru Avadanii <Alexandru.Avadanii@enea.com>
    (cherry picked from commit 45b22fab76f84cdbf71a6693dc42f24edaa4334e)

--- mcp/scripts/log.sh
+++ mcp/scripts/log.sh
@@ -14,6 +14,8 @@ DEPLOY_LOG=$1
 OPNFV_TMP_LOG="opnfv_fuel_logs"
 
 [ -n "${DEPLOY_LOG}" ] || exit 0
+# shellcheck disable=SC1091
+[ -n "${SSH_OPTS}" ] || source globals.sh
 
 # ssh to cfg01
 # shellcheck disable=SC2086,2087
