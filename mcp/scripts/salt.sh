044f49fd195526d8c00967fb9a0b6b1ab0a36c14
Michael Polenchuk <mpolenchuk@mirantis.com>
2017-10-16 14:24:13 +0400

    [ovs/dpdk] Return true on linux.network state
    linux.network state is expected to be down until node reboot
    to activate required hugepages therefore return true in any case.
    
    Change-Id: I61deac12b9ac212c5f79cdcce30cda8e5d24413b
    Signed-off-by: Michael Polenchuk <mpolenchuk@mirantis.com>

--- mcp/scripts/salt.sh
+++ mcp/scripts/salt.sh
@@ -64,7 +64,7 @@ ssh ${SSH_OPTS} "${SSH_SALT}" bash -s -e << SALT_INSTALL_END
   salt '*' state.apply salt | grep -Fq 'No response' && salt '*' state.apply salt
 
   salt -C 'I@salt:master' state.sls linux
-  salt -C '* and not cfg01*' state.sls linux
+  salt -C '* and not cfg01*' state.sls linux || true
   salt -C '* and not cfg01*' pkg.upgrade refresh=False
 
   salt '*' state.sls ntp
