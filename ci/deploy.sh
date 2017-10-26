95835f48cc4a65e5ccc1c48c94fa4caa51e67733
Taseer Ahmed <taseer94@gmail.com>
2017-10-26 12:25:25 +0500

    Add missing installation dependency
    - uuidgen is required for installation
    - Add installation package for both Debian and
      RHEL based linux distribitions
    
    Change-Id: I9c9bf04e64dc6be1b4e8fb92aea531eccbea33a5
    Signed-off-by: Taseer Ahmed <taseer94@gmail.com>

--- ci/deploy.sh
+++ ci/deploy.sh
@@ -265,9 +265,9 @@ pushd "${DEPLOY_DIR}" > /dev/null
 
 # Install required packages
 [ -n "$(command -v apt-get)" ] && sudo apt-get install -y \
-  git make rsync mkisofs curl virtinst cpu-checker qemu-kvm
+  git make rsync mkisofs curl virtinst cpu-checker qemu-kvm uuid-runtime
 [ -n "$(command -v yum)" ] && sudo yum install -y --skip-broken \
-  git make rsync genisoimage curl virt-install qemu-kvm
+  git make rsync genisoimage curl virt-install qemu-kvm util-linux
 
 if [ "$(uname -i)" = "aarch64" ]; then
   [ -n "$(command -v apt-get)" ] && sudo apt-get install -y vgabios && \
