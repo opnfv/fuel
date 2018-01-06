2a8e93b72c52f1e829863095abac26b60f5828f4
Alexandru Avadanii <Alexandru.Avadanii@enea.com>
2018-01-06 19:27:04 +0100

    deploy.sh: Print sysinfo
    JIRA: FUEL-323
    
    Change-Id: I0dcbcfbedc7e9ef013ca50dcc08f804323f91701
    Signed-off-by: Alexandru Avadanii <Alexandru.Avadanii@enea.com>

---
+++ mcp/scripts/sysinfo_print.sh
@@ -0,0 +1,24 @@
+#!/bin/bash -ex
+##############################################################################
+# Copyright (c) 2017 Mirantis Inc., Enea AB and others.
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the Apache License, Version 2.0
+# which accompanies this distribution, and is available at
+# http://www.apache.org/licenses/LICENSE-2.0
+##############################################################################
+#
+# Collect jump server system information for deploy debugging
+#
+
+# Network info
+brctl show
+
+# Distro & pkg info
+cat /etc/*-release
+uname -a
+git --version
+make --version
+qemu-nbd --version
+libvirtd --version || libvirt-bin --version
+virt-install --version
+python --version
