c1aeb82dd26ec19b438b39f9951bfc51679ad9cb
Alexandru Avadanii <Alexandru.Avadanii@enea.com>
2018-01-26 21:59:28 +0100

    [PDF] Switch to generate_config, unify templates
    - move bash template handling (previously expanded via `enbsubst`)
      to lib.sh;
    - move j2 template handling to lib.sh;
    - move virsh network templates to 'mcp/scripts/virsh_net' subdir;
    - switch virsh network templates from `envsubst` expansion to j2 and
      leverage generate_config.py, similar to PDF Fuel installer adapter;
    - add relevant runtime env vars (e.g. SALT_MASTER, MAAS_IP) on the fly
      to PDF, to consume them in templates like params coming from PDF;
    - parameterize virsh network definitions based on PDF (mgmt, public);
    
    JIRA: FUEL-322
    
    Change-Id: Ib94e78fc4f25797b9354a0552e884104da5d0003
    Signed-off-by: Alexandru Avadanii <Alexandru.Avadanii@enea.com>

--- mcp/scripts/lib.sh
+++ mcp/scripts/lib.sh
@@ -321,8 +321,9 @@ function create_networks {
       virsh net-undefine "${net}"
     fi
     # in case of custom network, host should already have the bridge in place
-    if [ -f "net_${net}.xml" ] && [ ! -d "/sys/class/net/${net}/bridge" ]; then
-      virsh net-define "net_${net}.xml"
+    if [ -f "virsh_net/net_${net}.xml" ] && \
+     [ ! -d "/sys/class/net/${net}/bridge" ]; then
+      virsh net-define "virsh_net/net_${net}.xml"
       virsh net-autostart "${net}"
       virsh net-start "${net}"
     fi
@@ -469,6 +470,46 @@ function do_sysctl_cfg {
 function get_nova_compute_pillar_data {
   local value=$(salt -C 'I@nova:compute and *01*' pillar.get _param:"${1}" --out yaml | cut -d ' ' -f2)
   if [ "${value}" != "''" ]; then
-    echo  ${value}
+    echo "${value}"
   fi
+}
+
+function do_templates() {
+  local git_repo_root=$1; shift
+  local lab_config_uri=$1; shift
+  local image_dir=$1; shift
+  local target_lab=$1; shift
+  local target_pod=$1; shift
+
+  RECLASS_CLUSTER_DIR=$(cd "${git_repo_root}/mcp/reclass/classes/cluster"; pwd)
+  PHAROS_GEN_CFG="./pharos/config/utils/generate_config.py"
+  PHAROS_INSTALLER_ADAPTER="./pharos/config/installers/fuel/pod_config.yml.j2"
+  BASE_CONFIG_PDF="${lab_config_uri}/labs/${target_lab}/${target_pod}.yaml"
+  BASE_CONFIG_IDF="${lab_config_uri}/labs/${target_lab}/idf-${target_pod}.yaml"
+  LOCAL_PDF="${image_dir}/$(basename "${BASE_CONFIG_PDF}")"
+  LOCAL_IDF="${image_dir}/$(basename "${BASE_CONFIG_IDF}")"
+  LOCAL_PDF_RECLASS="${image_dir}/pod_config.yml"
+
+  # Make sample PDF/IDF available via default lab-config (pharos submodule)
+  ln -sf "$(readlink -f "../config/labs/local")" "./pharos/labs/"
+
+  # Expand PDF + IDF to reclass input class (pod_config.yml)
+  if ! curl --create-dirs -o "${LOCAL_PDF}" "${BASE_CONFIG_PDF}"; then
+    notify_e "[ERROR] Could not retrieve PDF (Pod Descriptor File)!"
+  elif ! curl -o "${LOCAL_IDF}" "${BASE_CONFIG_IDF}"; then
+    notify_e "[ERROR] Could not retrieve IDF (Installer Descriptor File)!"
+  elif ! "${PHAROS_GEN_CFG}" -y "${LOCAL_PDF}" \
+      -j "${PHAROS_INSTALLER_ADAPTER}" > "${LOCAL_PDF_RECLASS}"; then
+    notify_e "[ERROR] Could not convert PDF+IDF to reclass model input!"
+  fi
+
+  # Expand reclass runtime.yml and virsh network templates
+  printenv | \
+    awk '/^(SALT|MCP|MAAS|CLUSTER).*=/ { gsub(/=/,": "); print }' >> "${LOCAL_PDF}"
+  find "${RECLASS_CLUSTER_DIR}" 'virsh_net' ./*j2 -name '*.j2' | \
+  while read -r tp; do
+    if ! "${PHAROS_GEN_CFG}" -y "${LOCAL_PDF}" -j "${tp}" > "${tp%.j2}"; then
+      notify_e "[ERROR] Could not convert PDF to network definitions!"
+    fi
+  done
 }
