c1a5d5b318e1500fa8e5dfb3eaa4f36135ef79e7
Alexandru Avadanii <Alexandru.Avadanii@enea.com>
2018-08-18 03:46:13 +0200

    [docker] Switch to containerized Salt Master
    * Refactor OPNFV salt-formulas mechanism to resemble upstream git
      structure:
      - git submodules: add new submodule for each formula we patch;
      - create salt-formula-x directories for OPNFV formulas;
      - move mcp/metadata/service contents to their each formula subdir;
      - use `make patches-import` for patches previously handled by
        patch.sh;
      - retire patch.sh
    * states: add virtual_init:
      - mostly based on old salt.sh, which is now obsolete;
      - exclude salt-master service restart (it would kill the container);
    * scenarios: cleanup (rm cfg01 virtual node def), adopt virtual_init;
    * reclass: align our model with prebuilt container's Salt config:
      - drop linux:network pillar data (handled by Docker);
      - stop applying linux.system state on cfg01;
      - align salt user homedir;
      - drop salt-formula packages (preprovisioned);
    * minor plumbing in deploy.sh and lib.sh;
    
    JIRA: FUEL-383
    
    Change-Id: I28708a9b399d3f19012212c71966ebda9d6fc0ac
    Signed-off-by: Alexandru Avadanii <Alexandru.Avadanii@enea.com>

--- mcp/scripts/lib.sh
+++ mcp/scripts/lib.sh
@@ -454,10 +454,7 @@ function create_vms {
 
 function update_mcpcontrol_network {
   # set static ip address for salt master node, MaaS node
-  local cmac=$(virsh domiflist cfg01 2>&1| awk '/mcpcontrol/ {print $5; exit}')
   local amac=$(virsh domiflist mas01 2>&1| awk '/mcpcontrol/ {print $5; exit}')
-  virsh net-update "mcpcontrol" add ip-dhcp-host \
-    "<host mac='${cmac}' name='cfg01' ip='${SALT_MASTER}'/>" --live --config
   [ -z "${amac}" ] || virsh net-update "mcpcontrol" add ip-dhcp-host \
     "<host mac='${amac}' name='mas01' ip='${MAAS_IP}'/>" --live --config
 }
@@ -487,6 +484,27 @@ function start_vms {
     virsh start "${node}"
     sleep $((RANDOM%5+1))
   done
+}
+
+function prepare_containers {
+  local image_dir=$1
+  [ -n "${image_dir}" ] || exit 1
+  [ -n "${MCP_REPO_ROOT_PATH}" ] || exit 1
+
+  docker-compose -f docker-compose/docker-compose.yaml down
+  sudo rm -rf "${image_dir}/salt"
+  mkdir -p "${image_dir}/salt/"{master.d,minion.d}
+  # salt state does not properly configure file_roots in master.conf, hard set it
+  sed -e 's/user: salt/user: root/' -e 's/auto_accept:/open_mode:/' \
+      "${MCP_REPO_ROOT_PATH}/docker/files/salt/master.conf" > \
+      "${image_dir}/salt/master.d/opnfv.conf"
+  echo 'master: localhost' > "${image_dir}/salt/minion.d/opnfv.conf"
+  cp "${MCP_REPO_ROOT_PATH}/mcp/scripts/docker-compose/files/hosts" \
+      "${image_dir}/hosts"
+}
+
+function start_containers {
+  docker-compose -f docker-compose/docker-compose.yaml up --quiet-pull -d
 }
 
 function check_connection {
@@ -589,5 +607,13 @@ function docker_install {
     rm get-docker.sh
     # On RHEL distros, the Docker service should be explicitly started
     sudo systemctl start docker
+  fi
+  # Distro-provided docker-compose might be simply broken (Ubuntu 16.04)
+  COMPOSE_VERSION='1.22.0'
+  COMPOSE_URL="https://github.com/docker/compose/releases/download/${COMPOSE_VERSION}"
+  COMPOSE_BIN='/usr/local/bin/docker-compose'
+  if ! docker-compose --version | grep -q -e "${COMPOSE_VERSION}"; then
+    sudo curl -L "${COMPOSE_URL}/docker-compose-$(uname -s)-$(uname -m)" -o "${COMPOSE_BIN}"
+    sudo chmod +x "${COMPOSE_BIN}"
   fi
 }
