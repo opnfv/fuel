74dc96d6951db70a065bfbbec200c66a19f81271
Alexandru Avadanii <Alexandru.Avadanii@enea.com>
2018-08-25 00:43:56 +0200

    [build.sh] Use pipenv run instead of shell
    JIRA: FUEL-383
    
    Change-Id: Ia1b58f9e7eefd9f3efc4b5241ec496c545ccee25
    Signed-off-by: Alexandru Avadanii <Alexandru.Avadanii@enea.com>

--- ci/build.sh
+++ ci/build.sh
@@ -70,16 +70,15 @@ pushd "${DOCKER_DIR}" > /dev/null
 
 pipenv --two
 pipenv install
-pipenv shell \
-  "invoke build saltmaster-reclass \
+pipenv run \
+  invoke build saltmaster-reclass \
     --require 'salt salt-formulas opnfv reclass tini-saltmaster' \
     --dist=ubuntu \
     --dist-rel=xenial \
     --formula-rev=nightly \
     --opnfv-tag='${DOCKER_TAG}' \
     --salt='stable 2017.7' \
-    ${DOCKER_PUSH}; \
-  exit"
+    ${DOCKER_PUSH}
 
 popd > /dev/null
 
