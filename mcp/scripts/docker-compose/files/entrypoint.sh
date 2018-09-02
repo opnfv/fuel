bc615428d1d1527d86a55654ae3dffa233d5d4db
Alexandru Avadanii <Alexandru.Avadanii@enea.com>
2018-09-02 09:13:35 +0200

    [docker] Preseed OPNFV formulas, reclass.system
    - patch: allow passing custom reclass.system source;
    - patch: allow passing custom formula sources;
    - patch: allow preseeding extra files inside container;
    - patch: use forked formula-fetch script that accepts local git
      fs URIs;
    - prepare OPNFV formulas for fetching during container build by
      masquerading them as individual git repos using git filter-branch;
    - formulas: add empty .travis.yml to bypass formula-fetch filtering;
    - entrypoint: cleanup obsolete formula, reclass.system injection;
    - use local HTTP server to avoid using COPY instructions during
      container build (not only it would increase the resulting image
      with 50M+, it also invalidates build caches due to git filter-branch
      creating slightly different repositories for OPNFV formulas);
    - mount docker volumes as read-only where applicable durin deploy;
    - mount reclass.cluster in /srv/salt instead of copying it each time;
    
    JIRA: FUEL-383
    
    Change-Id: I5bfed13475f34ee701a3239986c21b295e7e0aeb
    Signed-off-by: Alexandru Avadanii <Alexandru.Avadanii@enea.com>

--- mcp/scripts/docker-compose/files/entrypoint.sh
+++ mcp/scripts/docker-compose/files/entrypoint.sh
@@ -7,78 +7,33 @@
 # http://www.apache.org/licenses/LICENSE-2.0
 ##############################################################################
 
+# Preseed salt master & minion configuration
 mkdir -p /etc/salt/{master.d,minion.d,proxy.d}
-
-if [ ! -z "$SALT_EXT_PILLAR" ]; then
-    cp -avr "/tmp/${SALT_EXT_PILLAR}.conf" /etc/salt/master.d/
+if [ -n "$SALT_EXT_PILLAR" ]; then
+    cp -a "/tmp/${SALT_EXT_PILLAR}.conf" /etc/salt/master.d/
+    # salt state does not properly configure file_roots in master.conf
+    cp -a /root/fuel/mcp/scripts/docker-compose/files/opnfv_master.conf \
+          /etc/salt/master.d/opnfv_master.conf
+    echo 'master: localhost' > /etc/salt/minion.d/opnfv_slave.conf
 fi
 
+# set up keypair based SSH access for ubuntu user
 if [ ! -f /home/ubuntu/.ssh/authorized_keys ]; then
     install -D -o ubuntu /root/fuel/mcp/scripts/mcp.rsa.pub \
                          /home/ubuntu/.ssh/authorized_keys
 fi
 
+# overwrite hosts only on first container up, to preserve cluster nodes
 if ! grep -q localhost /etc/hosts; then
-    # overwrite hosts only on first container up, to preserve cluster nodes
     cp -a /root/fuel/mcp/scripts/docker-compose/files/hosts /etc/hosts
 fi
 
-# salt state does not properly configure file_roots in master.conf, hard set it
-cp -a /root/fuel/mcp/scripts/docker-compose/files/opnfv_master.conf \
-      /etc/salt/master.d/opnfv_master.conf
-echo 'master: localhost' > /etc/salt/minion.d/opnfv_slave.conf
-
-# NOTE: Most Salt and/or reclass tools have issues traversing Docker mounts
-# or detecting them as directories inside the container.
-# For now, let's do a lot of copy operations to bypass this.
-# Later, we will inject the OPNFV patched reclass model during image build.
-rm -rf /srv/salt/reclass/classes/*
-cp -ar /root/fuel/mcp/reclass/classes/* /srv/salt/reclass/classes
-cp -ar /root/fuel/mcp/reclass/nodes/* /srv/salt/reclass/nodes
+# Only copy the reclass node for the current scenario
+cp -a "/root/fuel/mcp/reclass/nodes/cfg01.${CLUSTER_DOMAIN}.yml" \
+      /srv/salt/reclass/nodes
 # Sensitive data should stay out of /root/fuel, which is exposed via Jenkins WS
 cp -a /root/pod_config.yml \
       /srv/salt/reclass/classes/cluster/all-mcp-arch-common/opnfv/pod_config.yml
-
-# OPNFV formulas
-prefix=/srv/salt/formula/salt-formulas
-rm -f /root/fuel/mcp/salt-formulas/*/.git
-cp -ar /root/fuel/mcp/salt-formulas/* ${prefix}/
-for formula in 'armband' 'opendaylight' 'tacker'; do
-    ln -sf /root/fuel/mcp/salt-formulas/salt-formula-${formula}/* \
-           /srv/salt/env/prd/
-done
-
-# Re-create classes.service links that we destroyed above
-for formula in ${prefix}/*; do
-    if [ -e "${formula}/metadata/service" ] && [[ ! $formula =~ \. ]]; then
-        ln -sf "${formula}/metadata/service" \
-               "/srv/salt/reclass/classes/service/${formula#${prefix}/salt-formula-}"
-    fi
-done
-
-# Create links for salt-formula-* packages to mimic git-style salt-formulas
-for artifact in /usr/share/salt-formulas/env/_*/*; do
-    ln -sf "${artifact}" "/srv/salt/env/prd/${artifact#/usr/share/salt-formulas/env/}"
-done
-for artifact in /usr/share/salt-formulas/env/*; do
-    if [[ ! ${artifact} =~ ^_ ]]; then
-        ln -sf "${artifact}" "/srv/salt/env/prd/$(basename ${artifact})"
-    fi
-done
-for formula in /usr/share/salt-formulas/reclass/service/*; do
-    ln -sf "${formula}" "/srv/salt/reclass/classes/service/$(basename ${formula})"
-done
-
-# Tini init system resembles upstart very much, but needs a little adjustment
-sed -i -e "s|return 'start/running' in |return 'is running' in |" \
-       -e "s|ret = _default_runlevel|return _default_runlevel|" \
-    /usr/lib/python2.7/dist-packages/salt/modules/upstart.py
-
-# Remove broken symlinks in /srv/salt, silences recurring warnings
-find -L /srv/salt /srv/salt/env/prd/_* -maxdepth 1 -type l -delete
-
-# Fix up any permissions after above file shuffling
-sudo chown root:root -R /srv/salt
 
 service ssh start
 service salt-minion start
